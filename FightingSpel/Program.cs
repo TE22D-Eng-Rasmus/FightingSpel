f3d6909e8617e53542ba73de7792e4fc5ad89db2
Rasmus Eng <rasmus.eng@elev.ga.ntig.se>
2023-09-26 11:08:25 +0200

    first commit

---
+++ FightingSpel/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Transactions;
+
+Console.WriteLine("Welcome to FightingGame \nEnter your name to START");
+string Name = Console.ReadLine();
+
+while (Name == "") {
+Console.WriteLine("Enter Your Name");
+Name = Console.ReadLine();
+}
+
+Console.WriteLine($"For our next fight we bring on a new fighter.\nIts {Name} against our one and only champion\nClone{Name}!!!!");
+Console.ReadLine();
