28a19dca3432a3cc5c2dbfbe2497acb388bbe9ee
Rasmus Eng <rasmus.eng@elev.ga.ntig.se>
2023-09-26 14:31:02 +0200

    börjat med random

--- FightingSpel/Program.cs
+++ FightingSpel/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 using System.Transactions;
 
+Random generator = new Random();
+
 Console.WriteLine("Welcome to FightingGame \nEnter your name to START");
 string Name = Console.ReadLine();
 
@@ -9,5 +11,21 @@ Console.WriteLine("Enter Your Name");
 Name = Console.ReadLine();
 }
 
-Console.WriteLine($"For our next fight we bring on a new fighter.\nIts {Name} against our one and only champion\nClone{Name}!!!!");
+Console.WriteLine($"For our next fight we bring on a new fighter.\nIts {Name} against our one and only champion\nClone{Name}!!!!\nThe Champion goes first");
+Thread.Sleep(2500);
+int r = generator.Next(10);
+
+if (r > 4) {
+Console.WriteLine($"Clone{Name} did {r} damage\nImpressive");
+Thread.Sleep(1500);
+}
+
+if (r < 5) {
+Console.WriteLine($"Clone{Name} did {r} damage\nI thought he could do better");
+Thread.Sleep(1500);
+}
+
+int r2 = generator.Next(10);
+
+Console.WriteLine($"Han gjorde {r2} skada");
 Console.ReadLine();
